1b5fa6f61bae3d61adccefdb19492c097e50a00b
kinley2007wangmo <kw8384457@gmail.com>
2026-03-17 23:26:14 +0600

    Update: to-do app

---
+++ todo-app/backend/server.js
@@ -0,0 +1,55 @@
+const express = require("express")
+const cors = require("cors")
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+let tasks = []
+
+// CREATE
+app.post("/tasks",(req,res)=>{
+ const task = {
+   id: Date.now(),
+   text: req.body.text
+ }
+ tasks.push(task)
+ res.json(task)
+})
+
+// READ
+app.get("/tasks",(req,res)=>{
+ res.json(tasks)
+})
+
+// UPDATE
+app.put("/tasks/:id",(req,res)=>{
+ const id = parseInt(req.params.id)
+
+ tasks = tasks.map(task=>{
+   if(task.id === id){
+     task.text = req.body.text
+   }
+   return task
+ })
+
+ res.json({message:"updated"})
+})
+
+// DELETE
+app.delete("/tasks/:id",(req,res)=>{
+ const id = parseInt(req.params.id)
+
+ tasks = tasks.filter(task=>task.id !== id)
+
+ res.json({message:"deleted"})
+})
+
+app.get("/",(req,res)=>{
+ res.send("Backend running")
+})
+
+const PORT = process.env.PORT || 5000
+app.listen(PORT, () => {
+  console.log("Server running on port", PORT)
+})
